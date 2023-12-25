724411ecfd0331bd44e456a5488a6d51bcaecc9a
adrianjadev <adrianjademanaday@gmail.com>
2023-12-26 04:17:02 +0800

    follow-up

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
                 >
-                    adrianjadev | [2:09:58/2:43:45]
+                    adrianjadev
                 </motion.span>
                 <div className="social">
                     <a href="#"><img src="/facebook.png" alt="facebook" /></a>
